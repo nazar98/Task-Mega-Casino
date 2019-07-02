d61d3cb784766c04a224393befc6d64b6c3d5627
Nazar Heryliv <nazarheryliv12@gmail.com>
2019-07-02 07:55:34 +0300

    Create class User and organize his functionslity

---
+++ User.js
@@ -0,0 +1,22 @@
+class User{   
+    constructor(name, money){
+        this.name = name;
+        this.money = money;        
+    }
+    getMoney(){
+        return this.money;
+    }
+    setMoney(money) {
+        if(typeof money === "number" && money > 0){
+            this.money += money;
+        } else {
+            throw "Incorect value when you set the money on your account";
+        }
+        return this;
+    }
+
+    play(machine, money)
+    {
+       machine.play(money);
+    }
+}
